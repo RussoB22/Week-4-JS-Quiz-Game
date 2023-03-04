da4d099f140897ee6fd64d6cfa4cc13dd1b5b76d
Russo Borneo <russo.borneo2013@gmail.com>
2023-03-04 15:43:05 -0600

    Attempted to get a leaderboard to work with the form fuction

--- scripts.js
+++ scripts.js
@@ -2,14 +2,25 @@ var timerEl = document.getElementById('startCountDown');
 var startQuiz = document.getElementById("startButton");
 var startCard = document.getElementById("startCard")
 var quizCard = document.getElementById("quizCard")
+var quizQuestion = document.getElementById("question")
 var choices = document.getElementsByClassName("choice")
-console.log(choices)
 var choice0 = document.getElementById("choice0")
 var choice1 = document.getElementById("choice1")
 var choice2 = document.getElementById("choice2")
 var choice3 = document.getElementById("choice3")
 var choice4 = document.getElementById("choice4")
+var score = document.getElementById("score")
+var results = document.getElementById("resultCard")
+var leaderButton = document.getElementById("leaderBoardButton")
+var leaderBoard = document.getElementById("leaderBoard")
+var highscoreFormEl = document.getElementById("gameScore")
+var highscoreListEl = document.getElementById("leaders")
+var highscorer = document.getElementById("gameScoreName").value
 var currentInterval
+var randomQuizQuestion
+var currentScore = 0
+
+// ountdown Clock Logic
 
 function startCountDown() {
     var timeLeft = 30;
@@ -27,19 +38,18 @@ function startCountDown() {
 }
 
 function quizCountDown() {
-    var timeLeft = 120;
+    // Change to 120
+    var timeLeft = 2;
     var timeInterval = setInterval(function () {
         timerEl.textContent = timeLeft + " seconds till emotional damage.";
         if(timeLeft === 0) {
         clearInterval(timeInterval);
+        quizCard.style.display = "none";
+        results.style.display = "block";
         }
         timeLeft--;
     }, 1000);
 }
-
-// startQuiz.addEventListener("click", function() {
-    
-//   })
 
 startQuiz.onclick = function() {
     startCard.style.display = "none";
@@ -50,60 +60,95 @@ startQuiz.onclick = function() {
 
 startCountDown();
 
+// Question Array/Object
+
 var questions = [
     {
-        question: "What is a local variable?",
+        question: "Fav Pet?",
         choices: [
-            {choice: "A variable within a function", 
+            {choice: "Dog", 
             isCorrect:true},
-            {choice: "a drink",
+            {choice: "Cat",
             isCorrect:false},
-            {choice: "coffee",
+            {choice: "Fish",
             isCorrect:false},
-            {choice: "a triagular shaped snack",
+            {choice: "Snake",
             isCorrect:false},
-            {choice: "pork",
+            {choice: "Rabbit",
             isCorrect:false}] 
     },
     {
-        question: "What is a local variable??",
+        question: "Fav Book?",
         choices: [
-            {choice: "A variable within a function", 
+            {choice: "Fiction", 
             isCorrect:true},
-            {choice: "a drink",
+            {choice: "Non-Fiction",
             isCorrect:false},
-            {choice: "coffee",
+            {choice: "Redacted",
             isCorrect:false},
-            {choice: "a triagular shaped snack",
+            {choice: "Weeb crap",
             isCorrect:false},
-            {choice: "pork",
+            {choice: "Where's Waldo",
             isCorrect:false}] 
     },
     {
-        question: "What is a local variable???",
+        question: "Fav Instument?",
         choices: [
-            {choice: "A variable within a function", 
+            {choice: "Guitar", 
             isCorrect:true},
-            {choice: "a drink",
+            {choice: "Bass",
             isCorrect:false},
-            {choice: "coffee",
+            {choice: "6 String Bass",
             isCorrect:false},
-            {choice: "a triagular shaped snack",
+            {choice: "Really Big Bass",
             isCorrect:false},
-            {choice: "pork",
+            {choice: "Electric Triangle",
+            isCorrect:false}] 
+    },
+    {
+        question: "Fav Snack?",
+        choices: [
+            {choice: "Pringles", 
+            isCorrect:true},
+            {choice: "Doritos",
+            isCorrect:false},
+            {choice: "Sun Chips",
+            isCorrect:false},
+            {choice: "Ghost Peppers",
+            isCorrect:false},
+            {choice: "Carolina Reapers",
+            isCorrect:false}] 
+    },
+    {
+        question: "Where is the Mascot of Russia's 2nd top national sport living?",
+        choices: [
+            {choice: "Moscow", 
+            isCorrect:true},
+            {choice: "Nadir",
+            isCorrect:false},
+            {choice: "Despia",
+            isCorrect:false},
+            {choice: "Albaz",
+            isCorrect:false},
+            {choice: "Why",
             isCorrect:false}] 
     }
 ]
+
+// Questions Logic
 
 function shuffle(arr) {
     arr.sort(() => Math.random() - 0.5);
 }
 
-function chosenquestion() {
-    shuffle(questions);
-    shuffle(this.choices);
-console.log(jsQuestion3.choices)
+function questionShuffle() {
+    shuffle(questions);    
 }
+
+questionShuffle();
+
+console.log(questions)
+console.log(questions[0])
 
 choice0.textContent = questions[0].choices[0].choice;
 choice0.classList.add("correct");
@@ -111,15 +156,89 @@ choice0.onclick = function() {
     
 }
 
-choices.onclick = function() {
-    alert("correct");
+choices.addEventListener('click', nextQuestion)
+
+function nextQuestion() {
+    i++;
+
+
 }
 
-// function getRandom(arr) {
-//     var randIndex = Math.floor(Math.random() * arr.length);
-//     var randElement = arr[randIndex];
-//     return randElement;
+// Highscore Form
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    var highscoreLeader = highscorer; 
+    var highscoreListItemEl = document.createElement('li');
+    var deleteButtonEl = document.createElement('button');
+
+    if (!highscoreLeader) {
+      console.log('Type in a Name.');
+      return;
+    }
+
+    highscoreListItemEl.text(highscoreLeader);
+  
+    // add delete button to remove shopping list item
+    highscoreListItemEl.appendChild(deleteButtonEl);
+  
+    // print to the page
+    highscoreListEl.append(highscoreListItemEl);
+  
+    // clear the form input element
+    document.querySelector('input[id="gameScoreName"]').value = '';
+
+    var highscoreForm = highscoreFormEl;
+        highscoreForm.addEventListener('submit', handleFormSubmit);
+
+  }
+
+// v2
+
+// function handleFormSubmit(event) {
+//     event.preventDefault();
+  
+//     var highscoreLeader = document.querySelector('input[id="gameScoreName"]').value;
+//     var highscoreListItemEl = document.createElement('li');
+//     var deleteButtonEl = document.createElement('button');
+  
+//     if (!highscoreLeader) {
+//       console.log('Type in a Name.');
+//       return;
+//     }
+  
+//     highscoreListItemEl.textContent = highscoreLeader;
+  
+//     // add delete button to remove highscore list item
+//     deleteButtonEl.className = 'btn btn-danger btn-small delete-item-btn';
+//     deleteButtonEl.textContent = 'Remove';
+//     highscoreListItemEl.appendChild(deleteButtonEl);
+  
+//     // print to the page
+//     var highscoreListEl = document.querySelector('#highscore-list');
+//     highscoreListEl.appendChild(highscoreListItemEl);
+  
+//     // clear the form input element
+//     document.querySelector('input[id="gameScoreName"]').value = '';
 //   }
+  
+//   var highscoreFormEl = document.querySelector('#highscore-form');
+//   highscoreFormEl.addEventListener('submit', handleFormSubmit);
 
+// Leaderboard
 
+leaderButton.addEventListener('click', showLeaders);
 
+function showLeaders() {
+    leaderBoard.style.display = "block";
+    leaderButton.style.display = "none";
+
+}
+
+leaderBoard.addEventListener('click', hideLeaders)
+
+function hideLeaders() {
+    leaderBoard.style.display = "none";
+    leaderButton.style.display = "block";
+}
